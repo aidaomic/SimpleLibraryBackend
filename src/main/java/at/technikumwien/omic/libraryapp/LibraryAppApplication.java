611f90237386eb7cc08b7fbe5b41c88fe86214c7
if20b204 <if20b204@technikum-wien.at>
2022-11-02 10:17:16 +0100

    initial commit

---
+++ src/main/java/at/technikumwien/omic/libraryapp/LibraryAppApplication.java
@@ -0,0 +1,13 @@
+package at.technikumwien.omic.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAppApplication.class, args);
+    }
+
+}
